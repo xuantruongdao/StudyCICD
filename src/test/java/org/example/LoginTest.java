38bad144181419e9860544b651cdccb8945dd7b4
xuantruongdao <truong88bk1@gmail.com>
2026-03-27 15:08:13 +0700

    Them bai test Login theo mo hinh POM

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -12,14 +12,37 @@ public class LoginTest {
     WebDriver driver;
     LoginPage loginPage;
 
+//    @BeforeMethod
+//    public void setup() {
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless=new"); // Bắt buộc để chạy trên Jenkins
+//        driver = new ChromeDriver(options);
+//        driver.get("https://the-internet.herokuapp.com/login");
+//        loginPage = new LoginPage(driver);
+//    }
     @BeforeMethod
     public void setup() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // Bắt buộc để chạy trên Jenkins
+
+        // Đọc biến môi trường "HEADLESS" từ hệ thống
+        // Nếu không tìm thấy biến này, mặc định sẽ là "false" (chạy có UI)
+        String headlessMode = System.getProperty("HEADLESS", "false");
+
+        if (headlessMode.equalsIgnoreCase("true")) {
+            options.addArguments("--headless=new");
+            System.out.println("--- Đang chạy ở chế độ HEADLESS (Jenkins) ---");
+        } else {
+            System.out.println("--- Đang chạy ở chế độ UI (Local) ---");
+        }
+
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
         driver.get("https://the-internet.herokuapp.com/login");
         loginPage = new LoginPage(driver);
     }
+
 
     @Test
     public void testSuccessfulLogin() {
